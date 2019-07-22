5ecdf114b6b08db93f4f44c6c0c6a468a47dabcc
Jamie Nelson <james.bnelson0@gmail.com>
2019-07-22 11:57:16 -0400

    update blog_id

--- webscraper.py
+++ webscraper.py
@@ -14,7 +14,7 @@ from firebase import add_workout
 # activate test environment --> .\env\Scripts\activate
 
 date = datetime.today()
-blog_id = 390943 - (date.day - 8)
+blog_id = 398530 - (date.day - 22)
 
 url = 'https://crossfit-toybox.triib.com/blog/2019-07-'+ str(date.day) + '/'+ str(blog_id) + '/'
 response = requests.get(url, timeout=5)
